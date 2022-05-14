05204157145e2c7b8bef1516d339d20fdcb9bf50
stonka123 <stonka123@gmail.com>
2022-05-14 14:06:14 +0200

    working route

--- src/js/modules/start.js
+++ src/js/modules/start.js
@@ -1,16 +1,15 @@
 const startBtn = document.querySelector('.container-start__text-button')
 
 export const startWork = () => {
-	const day = document.querySelector('.work-date')
-	const hour = document.getElementsByClassName('work-hour')
-
+	const day = document.getElementById('work-date')
+	const hour = document.getElementById('work-hour')
 	const today = new Date()
+
 	const startDate = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate()
 	const startHour = today.getHours() + ':' + today.getMinutes() + ':' + today.getSeconds()
 	day.innerHTML = startDate
 	hour.innerHTML = startHour
-
-	console.log(day)
+	console.log(startHour)
 }
 
 startBtn.addEventListener('click', startWork)
