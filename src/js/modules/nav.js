a029cfcc20f3686474a1629f5b76d71f7f853d50
stonka123 <stonka123@gmail.com>
2022-05-26 19:15:08 +0200

    change colors add closest

--- src/js/modules/nav.js
+++ src/js/modules/nav.js
@@ -8,6 +8,7 @@ const mainContent = document.querySelector('.main-content')
 const contentArea = document.querySelector('.content')
 const homeIcon = document.querySelector('.start-icon')
 const tasksIcon = document.querySelector('.tasks-icon')
+// const navItems = document.querySelectorAll('.nav-items__item')
 
 export const showStart = () => {
 	if (contentArea.classList.contains('start') === false) {
@@ -34,5 +35,26 @@ export const observeSection = e => {
 		homeIcon.classList.remove('icon-spy')
 	}
 }
+const navItems = [...document.querySelectorAll('.nav-icons')]
+
+document.querySelector('#main-nav').addEventListener('click', e => {
+	const closestElement = e.target.closest('.nav-icons')
+	const navTarget = e.target
+
+	console.log(navTarget)
+	if (!navTarget.classList.contains('nav-items')) {
+		clear()
+	}
+	setActive(closestElement)
+})
+
+function clear(closestElement) {
+	navItems.forEach(element => element.classList.remove('nav-spy'))
+}
+
+function setActive(closestElement) {
+	closestElement.classList.add('nav-spy')
+}
+
 navStart.addEventListener('click', showStart)
 navTasks.addEventListener('click', showTasks)
