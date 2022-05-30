65e20c4182c35bdf69dc22ea71988e38921d9335
stonka123 <stonka123@gmail.com>
2022-05-30 12:33:32 +0200

    addes section tasks

--- src/js/modules/panel-top.js
+++ src/js/modules/panel-top.js
@@ -4,7 +4,9 @@ import './shop.js'
 const panelTop = document.querySelector('.panel-box')
 const panelBurgerBtn = document.querySelector('.panel-burger')
 const notificationsIcon = document.querySelector('.panel-box__item-notifications')
+const basketIcon = document.querySelector('.panel-box__item-basket')
 const notifications = document.querySelector('.notifications')
+const basket = document.querySelector('.basket')
 const popupQuerySelector = '#popup'
 const popupEl = document.querySelector(popupQuerySelector)
 
@@ -30,17 +32,29 @@ export const showAvatar = avatar => {
 
 document.querySelector('.dashboard').addEventListener('click', e => {
 	const notiTarget = e.target
+	const notifications = document.querySelector('.notifications')
 	const closestNoti = e.target.closest('.panel-box__item-notifications')
+	const closestBasket = e.target.closest('.panel-box__item-basket')
 	const targetPopup = e.target.closest('.notifications')
+	const targetBasket = e.target.closest('.basket')
 
 	if (closestNoti) {
 		notifications.classList.toggle('notifications-show')
+		alert()
 	} else if (targetPopup) {
 		notifications.classList.add('notifications-show')
+		alert()
 	} else if (!closestNoti) {
 		notifications.classList.remove('notifications-show')
+		clear()
+	}
+
+	function alert() {
+		notificationsIcon.classList.add('alert')
+	}
+	function clear() {
+		notificationsIcon.classList.remove('alert')
 	}
 })
-
 
 panelBurgerBtn.addEventListener('click', showPanelIconMobile)
